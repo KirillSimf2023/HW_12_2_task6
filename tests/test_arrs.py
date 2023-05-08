37ecca98a917751960eb572f53dc0311726e9cd1
kirill <lobbi.kirill@mail.ru>
2023-05-08 13:03:18 +0300

    Добавил arrs_fixture и использовал её в функции test_get

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,8 +2,23 @@ from utils import arrs
 import pytest
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.fixture
+def arrs_fixture():
+    return [1, 2, 3, 4]
+
+def test_arrs_add(arrs_fixture):
+    assert arrs.arrs_add(arrs_fixture, 5, 6) == [1, 2, 3, 4, 5, 6]
+
+def test_arrs_add_2(arrs_fixture):
+    assert arrs.arrs_add(arrs_fixture, 5) == [1, 2, 3, 4, 5]
+
+def test_arrs_add_3(arrs_fixture):
+    assert arrs.arrs_add(arrs_fixture) == [1, 2, 3, 4]
+
+
+def test_get(arrs_fixture):
+    assert arrs.get(arrs_fixture, 1, "test") == 2
+    assert arrs.get(arrs_fixture, -1, "test") == "test"
     assert arrs.get([], -1, "test") == "test"
 
 
